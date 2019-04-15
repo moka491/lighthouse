fc319b404337a7a6af5633f7817e1d5ed7169ed6
Patrick Hulce <patrick.hulce@gmail.com>
2019-04-15 17:51:10 -0500

    core: rename diagnostics to debugData

--- lighthouse-core/audits/accessibility/axe-audit.js
+++ lighthouse-core/audits/accessibility/axe-audit.js
@@ -63,11 +63,11 @@ class AxeAudit extends Audit {
       {key: 'node', itemType: 'node', text: str_(UIStrings.failingElementsHeader)},
     ];
 
-    /** @type {LH.Audit.Details.Diagnostic|undefined} */
+    /** @type {LH.Audit.Details.DebugData|undefined} */
     let diagnostic;
     if (impact || tags) {
       diagnostic = {
-        type: 'diagnostic',
+        type: 'debug',
         impact,
         tags,
       };
@@ -78,7 +78,7 @@ class AxeAudit extends Audit {
       extendedInfo: {
         value: rule,
       },
-      details: {...Audit.makeTableDetails(headings, items), diagnostic},
+      details: {...Audit.makeTableDetails(headings, items), debug: diagnostic},
     };
   }
 }
